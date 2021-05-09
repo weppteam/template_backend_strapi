8a868feafc10a201bd9a9d7bc41aefea3ee25b9b
guilherme-humberto <gbreu@outlook.com>
2021-05-09 13:20:07 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '40aa7d29f22f197de5051af23f0eefbe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9030f24b-463b-4698-90e1-77f21f3a0e71'
+};
